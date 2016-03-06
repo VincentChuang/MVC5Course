66c07ae99fc0a4c7c97cfcee609e07e27cdbff5b
VincentChuang <laba.chuang@gmail.com>
2016-03-06 11:05:45 +0800

    練習對 FabricsEntities 的 SaveChanges() 進行例外處裡

--- MVC5Course/Controllers/EFController.cs
+++ MVC5Course/Controllers/EFController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
+using System.Data.Entity.Validation;
 
 namespace MVC5Course.Controllers {
     public class EFController : Controller {
@@ -14,12 +15,25 @@ namespace MVC5Course.Controllers {
             db.Product.Add(
                 new Product() {
                     ProductName = "BMW",
-                    Price = 5,
+                    Price = 1,
                     Stock = 1,
                     Active = true
                 }
             );
-            db.SaveChanges();
+
+            try {
+                db.SaveChanges();
+            } catch (DbEntityValidationException ex) {
+                string s = "";
+                foreach (var e1 in ex.EntityValidationErrors) {
+                    foreach (var e2 in e1.ValidationErrors) {
+                        s += ";驗證欄位=" + e2.PropertyName + ",失敗訊息=" + e2.ErrorMessage;
+                    }
+                }
+                if (s != "")
+                    s = s.Substring(1);
+                throw new Exception(s); //傳出自訂錯誤
+            }
 
             var data = db.Product.ToList();
 
