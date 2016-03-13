0a1ebf99345530f1cfe7ddff77bfe9f3fb28a6a3
VincentChuang <laba.chuang@gmail.com>
2016-03-13 11:02:21 +0800

    34 練習透過表單驗證 (FormsAuthentication) 實作登入、登出機制

--- MVC5Course/Controllers/HomeController.cs
+++ MVC5Course/Controllers/HomeController.cs
@@ -7,14 +7,14 @@ using System.Web.Mvc;
 namespace MVC5Course.Controllers {
     public class HomeController : BaseController {
 
-        [紀錄Action的執行時間]
-        [共用的ViewBag資料共享於部分HomeController動作方法]
+        [紀錄Action的執行時間]                                 //自訂 ActionFilter
+        [共用的ViewBag資料共享於部分HomeController動作方法]    //自訂 ActionFilter
         public ActionResult Index() {
             return View();
         }
 
-        [紀錄Action的執行時間]
-        [共用的ViewBag資料共享於部分HomeController動作方法]
+        [紀錄Action的執行時間]                                 //自訂 ActionFilter
+        [共用的ViewBag資料共享於部分HomeController動作方法]    //自訂 ActionFilter
         public ActionResult About() {
             ViewBag.Message = "Your application description page.";
             return View();
@@ -22,7 +22,6 @@ namespace MVC5Course.Controllers {
 
         public ActionResult Contact() {
             ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
---
+++ MVC5Course/Controllers/MemberController.cs
@@ -0,0 +1,43 @@
+﻿using MVC5Course.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVC5Course.Controllers
+{
+    [Authorize]
+    public class MemberController : Controller
+    {
+
+        [AllowAnonymous]    //排除不驗證
+        public ActionResult Login() {
+            return View();
+        }
+        [HttpPost]
+        [AllowAnonymous]    //排除不驗證
+        public ActionResult Login(LoginViewModel login) {
+            //驗證成功
+            if (CheckLogin(login.Email, login.Password)) {
+                FormsAuthentication.RedirectFromLoginPage(login.Email, false);
+                return RedirectToAction("Index","Home");
+            }
+
+            ModelState.AddModelError("Password","驗證錯誤");
+
+            return View();
+        }
+        private bool CheckLogin(string p1, string p2) {
+            return (p1 == "laba.chuang@gmail.com" && p2 == "123");
+        }
+        
+        /// <summary>登出</summary>
+        public ActionResult logout() {    //登出
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
