d375cd21c3f5a7c510c7371372825f52c8f19375
VincentChuang <laba.chuang@gmail.com>
2016-03-13 14:17:11 +0800

    37 使用 Bootstrap_Metro_Dashboard-master.zip 製作一份新的 Layout 頁面

---
+++ MVC5Course/Controllers/NewLayoutController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC5Course.Controllers {
+    public class NewLayoutController : Controller {
+
+        public ActionResult Index() {
+            return View();
+        }
+
+    }
+}
