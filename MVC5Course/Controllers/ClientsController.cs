45ffb1097a9c76a34f09bf491f6aabf8d68542ab
VincentChuang <laba.chuang@gmail.com>
2016-03-19 14:39:51 +0800

    49 將 /Client/Index 頁面 Ajax 化的 Edit 功能改成 Ajax.BeginForm 版本

--- MVC5Course/Controllers/ClientsController.cs
+++ MVC5Course/Controllers/ClientsController.cs
@@ -8,37 +8,31 @@ using System.Web;
 using System.Web.Mvc;
 using MVC5Course.Models;
 
-namespace MVC5Course.Controllers
-{
-    public class ClientsController : Controller
-    {
+namespace MVC5Course.Controllers {
+    public class ClientsController : Controller {
+
         private FabricsEntities db = new FabricsEntities();
 
         // GET: Clients
-        public ActionResult Index()
-        {
+        public ActionResult Index() {
             var client = db.Client.Include(c => c.Occupation);
             return View(client.Take(5).ToList());
         }
 
         // GET: Clients/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Details(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Client client = db.Client.Find(id);
-            if (client == null)
-            {
+            if (client == null) {
                 return HttpNotFound();
             }
             return View(client);
         }
 
         // GET: Clients/Create
-        public ActionResult Create()
-        {
+        public ActionResult Create() {
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName");
             return View();
         }
@@ -48,10 +42,8 @@ namespace MVC5Course.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Create([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client) {
+            if (ModelState.IsValid) {
                 db.Client.Add(client);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -62,15 +54,12 @@ namespace MVC5Course.Controllers
         }
 
         // GET: Clients/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Edit(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Client client = db.Client.Find(id);
-            if (client == null)
-            {
+            if (client == null) {
                 return HttpNotFound();
             }
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
@@ -82,28 +71,30 @@ namespace MVC5Course.Controllers
         // 詳細資訊，請參閱 http://go.microsoft.com/fwlink/?LinkId=317598。
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client)
-        {
-            if (ModelState.IsValid)
-            {
+        public ActionResult Edit([Bind(Include = "ClientId,FirstName,MiddleName,LastName,Gender,DateOfBirth,CreditRating,XCode,OccupationId,TelephoneNumber,Street1,Street2,City,ZipCode,Longitude,Latitude,Notes")] Client client) {
+            if (ModelState.IsValid) {
                 db.Entry(client).State = EntityState.Modified;
                 db.SaveChanges();
-                return RedirectToAction("Index");
+                
+                //return RedirectToAction("Index");
+
+                //原返回 redirect 轉址，
+                //後改為 輸出 Index View 畫面
+                //以符合 Ajax
+                return View("Index", db.Client.Include(c => c.Occupation).Take(5));
+
             }
             ViewBag.OccupationId = new SelectList(db.Occupation, "OccupationId", "OccupationName", client.OccupationId);
             return View(client);
         }
 
         // GET: Clients/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
+        public ActionResult Delete(int? id) {
+            if (id == null) {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
             Client client = db.Client.Find(id);
-            if (client == null)
-            {
+            if (client == null) {
                 return HttpNotFound();
             }
             return View(client);
@@ -112,21 +103,19 @@ namespace MVC5Course.Controllers
         // POST: Clients/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
+        public ActionResult DeleteConfirmed(int id) {
             Client client = db.Client.Find(id);
             db.Client.Remove(client);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
+        protected override void Dispose(bool disposing) {
+            if (disposing) {
                 db.Dispose();
             }
             base.Dispose(disposing);
         }
+
     }
 }
